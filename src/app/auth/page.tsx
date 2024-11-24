c8275945c25202bfab0141ddcd9c3c96d3a343c1
volodymyr_korolchuk <gitpub10@gmail.com>
2024-11-24 14:46:04 +0200

    chore: setup routes and fonts

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Auth = () => {
+  return <div>Register and Login forms go here</div>;
+};
+
+export default Auth;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button>Some button text</Button>
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique ex dolor culpa accusamus placeat sit aliquam molestias fugit iusto quasi!
+      <Button>Some button text</Button>
+    </div>
   );
 }
